9f6c91a708b12e9b86ddf961aac54b072d9a73ba
janry <janryseran069407@gmail.com>
2026-01-21 14:12:34 +0800

    updated role seeders

--- database/seeders/RoleUsersSeeder.php
+++ database/seeders/RoleUsersSeeder.php
@@ -11,19 +11,25 @@ class RoleUsersSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'itadmin@ndmu.edu.ph'],
+            ['email' => 'imis@ndmu.edu.ph'],
             [
                 'name' => 'IT Admin',
-                'password' => Hash::make('ChangeMe123!'),
+                'first_name' => 'MIS',
+                'last_name' => 'Admin',
+                'middle_name' => '',
+                'password' => Hash::make('@m1s@Office'),
                 'role' => 'it_admin',
             ]
         );
 
         User::updateOrCreate(
-            ['email' => 'alumni.officer@ndmu.edu.ph'],
+            ['email' => 'alumni@ndmu.edu.ph'],
             [
                 'name' => 'Alumni Officer',
-                'password' => Hash::make('ChangeMe123!'),
+                'first_name' => 'Alumni',
+                'last_name' => 'Officer',
+                'middle_name' => '',
+                'password' => Hash::make('@Alumni123@Office'),
                 'role' => 'alumni_officer',
             ]
         );
